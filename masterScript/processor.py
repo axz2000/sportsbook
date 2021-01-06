e864247af6761946db4402a02c30b85ccb306e0d
Will Shamma <willshamma@Wills-MacBook-Air.local>
2021-01-06 01:58:36 -0500

    Daily update

--- masterScript/processor.py
+++ masterScript/processor.py
@@ -1,4 +1,4 @@
-import ELO, EPL, NBA, FL1, GPL, NFL, FL2, KHL
+import ELO, EPL, NBA, FL1, GPL, NFL, FL2, KHL, TSL, ISA, LLA
 
 def getPicks():
 	NBA.run()
@@ -8,6 +8,9 @@ def getPicks():
 	GPL.run()
 	FL1.run()
 	FL2.run()
+	TSL.run()
+	ISA.run()
+	LLA.run()
 	return 'Picks Completed'
 	
 print('All bets are moneyline bets.')
