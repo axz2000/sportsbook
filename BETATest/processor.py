e55fb5a7a2b810908525f8415d77316abf241386
Will Shamma <willshamma@Wills-MacBook-Air.local>
2021-02-08 01:24:45 -0500

    Daily 2/8

--- BETATest/processor.py
+++ BETATest/processor.py
@@ -1,9 +1,9 @@
-import ELO, EPL, FL1, GPL, TSL, ISA, LLA, NBA, NPL, PPL, NFL, BPL, MLM, UCL, DSL
+import ELO, EPL, FL1, GPL, TSL, ISA, LLA, NBA, NPL, PPL, NFL, BPL, MLM, UCL, DSL, ECL
 
 #KHL when do we bring this back?
 
 def getPicks():
-	print(NBA.run())
+	'''print(NBA.run())
 	print(ELO.run())
 	print(EPL.run())
 	print(GPL.run())
@@ -13,12 +13,13 @@ def getPicks():
 	print(LLA.run())
 	print(NPL.run())
 	print(PPL.run())
-	print(NFL.run())
+	print(NFL.run())'''
 	print(BPL.run())
 	print(UCL.run())
 	print(MLM.run())
 	print(DSL.run())
-	#print(NHL.run())
+	print(ECL.run())
+	print(UCL.run())
 	return 'Picks Completed'
 	
 print('All bets are moneyline bets.')
