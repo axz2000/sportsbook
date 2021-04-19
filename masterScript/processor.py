af83c5d58954e74ad4b5997089645f042b2859d0
William Shamma <william.shamma2@gmail.com>
2021-04-19 00:37:43 -0400

    Update

--- masterScript/processor.py
+++ masterScript/processor.py
@@ -2,36 +2,44 @@ import ELO, EPL, FL1, GPL, TSL, ISA, LLA, NBA, NPL, PPL, NFL, BPL, ECL, MLM, UCL
 
 
 def getPicks():
-    NBA.run()
-    MLB.run()
-    
-    #NHL.run()
-    #NFL.run()
 	
-    ELO.run()
-    EPL.run()
-    GPL.run()
-    FL1.run()
-    TSL.run()
-    ISA.run()
-    LLA.run()
-    NPL.run()
-    #PPL.run()
-    ECL.run()
-    #MLM.run()
-    UCL.run()
-    #DSL.run()
-    EUL.run()
-    LLT.run()
-    #AAL.run()
-    FDA.run()
-    #BPL.run()
-    
-    MMA.run()
-    return 'Picks Completed'
+	try:
+		NBA.run()
+	except:
+		print('No NBA yet')
+	
+	try:
+		MLB.run()
+	except:
+		print('No MLB yet')
+	
+	try:
+		NHL.run()
+	except:
+		print('No NHL yet')
+
+	#NFL.run()
+	ELO.run()
+	EPL.run()
+	GPL.run()
+	FL1.run()
+	TSL.run()
+	ISA.run()
+	LLA.run()
+	NPL.run()
+	#PPL.run()
+	ECL.run()
+	#MLM.run()
+	UCL.run()
+	#DSL.run()
+	EUL.run()
+	LLT.run()
+	#AAL.run()
+	FDA.run()
+	#BPL.run()
+	MMA.run()
+	
+	return 'Picks Completed'
 	
 print('All bets are moneyline bets.')
 print(getPicks())
-
-
-	
